e8a429af5134f15f11673c04ec1476b6472bcaed
LBolechow <110845720+LBolechow@users.noreply.github.com>
2022-09-15 17:02:43 +0200

    Delete TimeTracking-main directory

--- TimeTracking-main/src/main/java/pl/lukbol/TimeTracking/repositories/TimeEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.lukbol.TimeTracking.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.lukbol.TimeTracking.models.TimeEntry;
-
-public interface TimeEntryRepository extends JpaRepository<TimeEntry, Long> {
-
-}
--- TimeTracking-main/src/main/java/pl/lukbol/TimeTracking/repositories/WorkdayRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.lukbol.TimeTracking.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.lukbol.TimeTracking.models.Workday;
-
-public interface WorkdayRepository extends JpaRepository<Workday, Long> {
-
-}
