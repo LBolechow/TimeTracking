432dc94074d75da95f9518c090591873d5021456
LBolechow <110845720+LBolechow@users.noreply.github.com>
2022-09-19 03:20:29 +0200

    Update

--- src/test/java/pl/lukbol/TimeTracking/TimeTrackingApplicationTests.java
+++ src/test/java/pl/lukbol/TimeTracking/TimeTrackingApplicationTests.java
@@ -39,7 +39,7 @@ public class TimeTrackingApplicationTests {
 	public void AddWorkDay() throws Exception {
 		this.mvc.perform(post("/workdays")
 						.contentType(MediaType.APPLICATION_JSON)
-						.content("{\"date\": \"2023\"}"))
+						.content("{\"date\": \"2022\"}"))
 				.andDo(print())
 				.andExpect(status().is2xxSuccessful());
 
